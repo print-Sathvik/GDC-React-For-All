a23be78f2b8017b55f464e4e5476fc6977d38a04
unknown <satvikreddy2017@gmail.com>
2023-04-09 01:55:49 +0530

    L1 Changed css changed to tailwind

--- src/FieldSet.tsx
+++ src/FieldSet.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./styles.css";
 
 export default function FieldSet(props: {
   id: number;
@@ -7,10 +6,19 @@ export default function FieldSet(props: {
   label: string;
 }) {
   return (
-    <div key={props.id} className="inputSet relative w-full mt-[35px]">
-      <input type={props.type} required={true} />
-      <label>{props.label}</label>
-      <i></i>
+    <div
+      key={props.id}
+      className="inputSet relative w-full mt-[35px] first:valid:"
+    >
+      <input
+        type={props.type}
+        required={true}
+        className="peer relative w-full pt-5 px-2.5 pb-2.5 bg-transparent outline-none z-[1] invalid:text-transparent focus:text-black duration-500"
+      />
+      <label className="absolute left-0 text-[#8f8f8f] pt-5 px-2.5 pb-2.5 peer-hover:text-[#45f3ff] peer-focus:text-[#45f3ff] peer-valid:text-[#45f3ff] peer-focus:-translate-y-8 peer-valid:-translate-y-8 peer-focus:text-[12px] peer-valid:text-[12px] duration-500">
+        {props.label}
+      </label>
+      <i className="peer-focus:h-11 peer-valid:h-11 absolute left-0 bottom-0 w-full h-0.5 rounded bg-[#45f3ff] duration-500"></i>
     </div>
   );
 }
